7c8dad222ba03374fe42d6248890ec80e9a49d3d
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2023-11-10 23:27:00 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -19,4 +19,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Join Backup', url='https://t.me/rajatfiles')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
